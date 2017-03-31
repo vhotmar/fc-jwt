edce97cf039bfe80df6ce95052b7d97decb5194a
Vojtěch Hotmar <vojta.hotmar@gmail.com>
2017-03-31 21:09:51 +0200

    update

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const DOMAIN = 'localhost';
 const PORT = process.env.PORT || 3000;
-const CALLBACK_URL = process.env.CALLBACK_URL || `http://${DOMAIN}:${PORT}`;
+const CALLBACK_URL = process.env.CALLBACK_URL || `http://${DOMAIN}:${PORT}/`;
 
 export default {
     domain: DOMAIN,
@@ -12,12 +12,12 @@ export default {
         audience: DOMAIN
     },
     steam: {
-        returnUrl: `${CALLBACK_URL}/auth/steam/return`,
+        returnUrl: `${CALLBACK_URL}auth/steam/return`,
         realm: `${CALLBACK_URL}`,
         apiKey: process.env.STEAM_API_KEY || '706A63DB54661CEFD21620FB0CDE6586'
     },
     facebook: {
-        callbackUrl: `${CALLBACK_URL}/auth/facebook/callback`,
+        callbackUrl: `${CALLBACK_URL}auth/facebook/callback`,
         appSecret: process.env.FACEBOOK_APP_SECRET || '318a826cdcfb3cd6a611b9024ca59a4b',
         appId: process.env.FACEBOOK_APP_ID || '1724095694547047'
     }
